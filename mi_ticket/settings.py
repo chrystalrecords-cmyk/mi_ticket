114bb07be4a92f853aa8bc9e9a891fcfd3b7a03f
chrystalrecords-cmyk <chrystalrecords@gmail.com>
2026-08-03 09:35:11 -0300

    Corregir settings.py y agregar dependencias

--- mi_ticket/settings.py
+++ mi_ticket/settings.py
@@ -86,3 +86,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
