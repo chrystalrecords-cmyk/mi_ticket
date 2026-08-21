da550f23c3c5a3beaa095d59d4836f5f6f7969be
chrystalrecords-cmyk <chrystalrecords@gmail.com>
2026-08-21 20:32:26 -0300

    Relacion de organizador integrada con exito

--- eventos/models.py
+++ eventos/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Evento(models.Model):
+    organizador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField()
     lugar = models.CharField(max_length=200)
