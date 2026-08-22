4fe64564c5ab5f43c40ee9acb64327daedb3eb89
chrystalrecords-cmyk <chrystalrecords@gmail.com>
2026-08-22 11:17:49 -0300

    Conexion permanente a Supabase PostgreSQL

--- mi_ticket/settings.py
+++ mi_ticket/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -51,12 +52,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mi_ticket.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
