a57f00a109997f76cf94a608a2bbc7f80614fa28
chrystalrecords-cmyk <chrystalrecords@gmail.com>
2026-09-03 18:31:44 -0300

    Corrijo indentacion en admin.py

--- eventos/admin.py
+++ eventos/admin.py
@@ -10,6 +10,6 @@ class OrdenAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre_comprador', 'email_comprador', 'evento', 'cantidad', 'monto_total', 'estado_pago', 'fecha_creacion')
     list_filter = ('estado_pago', 'fecha_creacion', 'evento')
     search_fields = ('nombre_comprador', 'email_comprador', 'id')
-    @admin.register(Producto)
+@admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'precio')
