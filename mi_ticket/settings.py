f02d5c913682920b9b729daec2bf94641e99d4f8
chrystalrecords-cmyk <chrystalrecords@gmail.com>
2026-09-04 08:46:29 -0300

    Corregido nombre de variable EMAIL_HOST_PASSWORD

--- mi_ticket/settings.py
+++ mi_ticket/settings.py
@@ -100,5 +100,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'soportemiticket@gmail.com'
-EMAIL_HOST_PASSWORD = 'ajassqrbrmaawipo'  # Sin espacios entre las letras
+EMAIL_HOST_PASSWORD = 'ajassqrbrmaawipo'
 DEFAULT_FROM_EMAIL = 'Mi Ticket <soportemiticket@gmail.com>'
